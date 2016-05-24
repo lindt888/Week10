9226a67e02ecb72a3063d94da896646753998bd3
lindt888 <jhowfeekoy@gmail.com>
2016-05-24 12:18:14 +0800

    Workshop9 test

---
+++ week10Test.py
@@ -0,0 +1,18 @@
+#create a new type of exception
+class Low_Inventory_Exception(Exception):
+    def __init__(self):
+        print("The inventory is low!")
+
+def test():
+    inventory = 11
+    if inventory < 10:
+        raise Low_Inventory_Exception
+
+try:
+    test()
+except(Low_Inventory_Exception):
+    print("Something is not right.")
+else:
+    print("Everything is good.")
+finally:
+    print("Everything is fixe
